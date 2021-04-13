d1ad81618c1bce1cef8394cd6ff4ee39975dcfda
Karina <karinaarudiak@gmail.com>
2021-04-13 19:35:55 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'stopwatch';
+  title = 'Stopwatch';
+  
+  interval;
+  timer = new Date(null);
+  isRunning = false;
+
+
+  startTimer(){
+    if (!this.isRunning) {
+      this.interval = setInterval(() => {
+        this.timer.setSeconds(this.timer.getSeconds() + 1)
+      })
+    }
+      else {
+      clearInterval(this.interval);
+      this.timer.setTime(0);
+    }
+    this.isRunning = !this.isRunning;
+  }
+
+  pauseTimer() {
+    if (this.isRunning) {
+      clearInterval(this.interval);
+      this.isRunning = !this.isRunning;
+    }
+  }
+
+  resetTimer() {
+    this.timer.setTime(0);
+  }
 }
